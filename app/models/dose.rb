8b38cb5078b160e2dd6be20def4bbc10f87c9b1d
Luciano Krebs <luciano.krebs@hotmail.com>
2017-08-03 19:45:12 -0300

    improving code

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -5,21 +5,3 @@ class Dose < ApplicationRecord
   validates :description, presence: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
-
-
-# cocktail
-# |id|name|
-# | 1|bloody mary|
-# | 2|screwdriver|
-
-# doses
-# |ingredient_id|cocktail_id|description|
-# |            1|          1|       50ml|
-# |            2|          1|       80ml|
-# |            1|          2|       50ml|
-
-# ingredients
-# |id|name|
-# | 1|vodka|
-# | 2|suco de tomate|
-# | 3|suco de laranja|
